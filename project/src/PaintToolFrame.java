83fddc40f6d56aa8edb0196c54cee0ae18ce7f16
Seunghwa <banksemi@naver.com>
2018-12-01 09:51:51 +0900

    Revert "맵에 안티얼리이싱 적용"
    This reverts commit 04393fdc6c8558f723ceda0a3ed314a54bdef9ba.

--- project/src/PaintToolFrame.java
+++ project/src/PaintToolFrame.java
@@ -48,9 +48,7 @@ public class PaintToolFrame extends JComponent{
 	
 	public void paintComponent(Graphics g)
 	{
-		g2 = (Graphics2D) g; 
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+		g2 = (Graphics2D) g;
 		offset_size = 2;
 		color = new Color(255,255,255);
 	
